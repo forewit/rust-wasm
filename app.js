9b4fd0330615c880ad0ea53d93ab340ca9b18c8c
Forewit <marcda495@gmail.com>
2021-08-20 05:41:51 -0700

    na

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-console.log("app.js is loaded...")
-
+// imports
 import init, * as rust from './pkg/rust_wasm.js';
 
-async function run() {
-    await init();
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+        typeof define === 'function' && define.amd ? define(['exports'], factory) :
+            (global = global || self, factory(global.app = {}));
+}(this, (function (exports) {
+    'use strict';
 
-    // And afterwards we can use all the functionality defined in wasm.
-    rust.greet("Marc");
-}
+    // do something
+    async function run() {
+        await init();
 
-run();
+        // And afterwards we can use all the functionality defined in wasm.
+        rust.greet("Marc");
+    }
+    run();
+
+    // define global exports
+    exports.rust = rust;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+})));
